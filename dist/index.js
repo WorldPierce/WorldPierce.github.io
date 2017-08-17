e805ce882aa843362156aacdad04839cb2c5057e
worldpierce <bildo316@gmail.com>
2017-08-17 15:52:55 -0400

    nav bar

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 var myIndex = 0;
+$('.navbar').hide();
 carousel();
+var element_position = $('#header_nav2').offset().top;
 
 function carousel() {
     var i;
@@ -12,3 +14,43 @@ function carousel() {
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 110); // Change image every 4 seconds
 }
+
+$(function(){
+    $('#header_nav').data('size','big');
+});
+
+$(window).scroll(function(){
+	//var y_scroll_pos = window.pageYOffset;
+    //var scroll_pos_test = element_position;
+    if($(document).scrollTop() > element_position)
+    {
+        if($('#header_nav').data('size') == 'big')
+        {
+
+        	$('.navbar').show();
+            $('#header_nav').data('size','small');
+            $('#header_nav').addClass('header_nav');
+            $('#header_nav').addClass('animated fadeIn');
+            // $('#header_nav').removeClass('fadeOut');
+            $('#header_nav').stop().animate({
+                
+            },600);
+        }
+    }
+    else
+    {
+        if($('#header_nav').data('size') == 'small')
+        {
+        	// $('header_nav').hide();
+        	// $('#header_nav').removeClass('header_nav');
+        	// $('#header_nav').addClass('fadeOut');
+        	$('#header_nav').removeClass('fadeIn');
+        	
+            $('#header_nav').data('size','big');
+            $('#header_nav').stop().animate({
+                
+            },600);
+            $('.navbar').hide("fast");
+        }  
+    }
+});
