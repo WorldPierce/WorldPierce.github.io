a25eb0514c1106ae4401c42c74f9161ece5fdf80
worldpierce <bildo316@gmail.com>
2017-08-19 13:02:56 -0400

    nav bar display change

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 var myIndex = 0;
-$('.nav').hide();
+// $('.nav').hide();
 carousel();
 var element_position = $('#header_nav2').offset().top;
 
@@ -20,8 +20,6 @@ $(function(){
 });
 
 $(window).scroll(function(){
-	//var y_scroll_pos = window.pageYOffset;
-    //var scroll_pos_test = element_position;
     if($(document).scrollTop() > element_position)
     {
         if($('#header_nav').data('size') == 'big')
@@ -32,26 +30,16 @@ $(window).scroll(function(){
             $('#header_nav').addClass('header_nav');
             $('#header_nav').addClass('animated fadeIn');
             $('#header_nav').removeClass('fadeOutUp');
-            // $('#header_nav').stop().animate({
-                
-            // },600);
+
         }
     }
     else
     {
         if($('#header_nav').data('size') == 'small')
         {
-        	// $('header_nav').hide();
-        	// $('#header_nav').removeClass('header_nav');
         	$('#header_nav').addClass('fadeOutUp');
         	$('#header_nav').removeClass('fadeIn');
-        	
             $('#header_nav').data('size','big');
-            // $('#header_nav').stop().animate({
-                
-            // },600);
-            //setTimeout(function(){ $('.nav').hide();}, 600);
-            
         }  
     }
 });
